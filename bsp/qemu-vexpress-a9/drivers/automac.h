fa7e4abd27650182e1069293b16527bcd4b79fdc
lisichonglt <lisichonglt@163.com>
2026-08-13 23:45:50 +0800

    qemu_a9 scons ok

--- bsp/qemu-vexpress-a9/drivers/automac.h
+++ bsp/qemu-vexpress-a9/drivers/automac.h
@@ -8,8 +8,8 @@
 #define AUTOMAC0  0x52
 #define AUTOMAC1  0x54
 #define AUTOMAC2  0x00
-#define AUTOMAC3  0x08
-#define AUTOMAC4  0xeb
-#define AUTOMAC5  0x33
+#define AUTOMAC3  0x2c
+#define AUTOMAC4  0x6d
+#define AUTOMAC5  0xb0
 
 #endif
